9637d61f43c9829f6ae03ae0e6c2ce688d660608
sk5s <samko5sam@tutanota.com>
2025-01-15 16:52:26 +0800

    💄 Add FAB on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,13 +3,22 @@ import MainReadList from '../components/MainReadList';
 import GeneralPage from './Layout/GeneralPage';
 import { useContext } from 'react';
 import { SettingsContext } from '../SettingsContext';
+import { IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
+import { useHistory } from 'react-router';
 
 function Home() {
   const {t} = useTranslation()
   const context = useContext(SettingsContext)
+  const history = useHistory()
   return (
     <GeneralPage title={t("app.name")} menuId="menu-home">
       <MainReadList list={context.defaultReadView} />
+      <IonFab slot="fixed" vertical="bottom" horizontal="end">
+        <IonFabButton onClick={() => {history.push("/add")}} color="dark">
+          <IonIcon icon={add}></IonIcon>
+        </IonFabButton>
+      </IonFab>
     </GeneralPage>
   );
 }
